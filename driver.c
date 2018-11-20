62a9bab4909f3f544cd7d3cfd57922e78b07c2c9
William Lu <rlu1@stuy.edu>
2018-11-20 11:02:50 -0500

    ?

--- driver.c
+++ driver.c
@@ -2,26 +2,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
 #include "helper.h"
 
 int main(){
   char* input = malloc(64);
   char* cwd = malloc(256);
+  char** args;
   int f, status;
+  // char broke;
 
   while (1){
-    f = fork();
-    if (!f)
-      break;
-    wait(&status);
+
+    getcwd(cwd, 256);
+    printf("%s> ", cwd);
+    scanf("%[^\n]s", input);
+    args = parse_args( input, " " );
+    // broke = 0;
+
+    if (!strcmp(args[0], "cd"))
+      chdir(args[1]);
+
+    else{
+      f = fork();
+      if (!f){
+        execvp(args[0], args);
+      }
+      else{
+        wait(&status);
+      }
+    }
+    // if (broke)
+    //   break;
+
+    // wait(&status);
+
     free(input);
+    free(cwd);
+    // free(args);
   }
 
-  getcwd(cwd, 256);
-  printf("%s> ", cwd);
-  scanf("%[^\n]s", input);
-  execute(input, cwd);
-  free(cwd);
+  // sleep(1.5);
+
   return 0;
 }
--- helper.c
+++ helper.c
@@ -1,12 +1,10 @@
 #include "helper.h"
 
-int execute(char* input, char* dir){
-  char ** args = parse_args( input, " " );
-  if ( !strcmp(args[0], "cd") )
-    return cd(args[1], dir);
-  execvp(args[0], args);
-  return 0;
-}
+// int execute(char* input, char* dir){
+//
+//   execvp(args[0], args);
+//   return 0;
+// }
 
 int cd(char* arg, char* dir){
   char ** new = parse_args(arg, "/");
@@ -14,7 +12,7 @@ int cd(char* arg, char* dir){
 }
 
 char** parse_args(char* line, char* delim){
-  char** args = calloc(1,6 sizeof(char*));
+  char** args = calloc(16, sizeof(char*));
   int i = 0;
   while (line){
     args[i] = strsep(&line, delim);
